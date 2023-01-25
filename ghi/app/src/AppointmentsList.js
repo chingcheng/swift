75090c2bfd1cc0251f748c138e23c2f076b24d0c
Ching Cheng <ching@chings-air.lan>
2023-01-24 17:40:54 -0800

    finished button done

--- ghi/app/src/AppointmentsList.js
+++ ghi/app/src/AppointmentsList.js
@@ -11,11 +11,27 @@ export default function AppointmentsList( {appointments, getAppointments } ) {
         return null;
     }
 
-
+    const finishAppointment = async (appointment) => {
+        const url = `http://localhost:8080/api/appointments/${appointment.id}/`
+        const fetchConfig = {
+            method: "put",
+            body: JSON.stringify({ finished: true}),
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+        const response = await fetch(url, fetchConfig)
+        if (response.ok) {
+            getAppointments()
+        }
+    }
 
 
     return (
-
+    <>
+      <div className='p-5 text-left bg-light'>
+        <h1 className='mb-3'>Service appointments</h1>
+        </div>
         <table className="table table-striped">
             <thead>
             <tr>
@@ -30,6 +46,7 @@ export default function AppointmentsList( {appointments, getAppointments } ) {
             </thead>
             <tbody>
             {appointments.map(appointment => {
+                if  (appointment.finished === false)
                 return (
                 <tr key={appointment.id}>
                     <td>{ appointment.vin }</td>
@@ -51,6 +68,7 @@ export default function AppointmentsList( {appointments, getAppointments } ) {
                     </td>
                     <td>
                         <button
+                            id={ appointment.id } onClick={() => finishAppointment(appointment)}
                             type="button" className="btn btn-info">
                             Finished
                         </button>
@@ -60,5 +78,6 @@ export default function AppointmentsList( {appointments, getAppointments } ) {
             })}
             </tbody>
         </table>
+    </>
     );
 }
